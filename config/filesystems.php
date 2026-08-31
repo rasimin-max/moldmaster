b1887df42401dd2d5f43b09ed406d84fae115420
rasimin-max <rasimin85ori@gmail.com>
2026-08-31 20:52:46 +0700

    Fix undefined array key cloud in cloudinary disk config

--- config/filesystems.php
+++ config/filesystems.php
@@ -64,6 +64,10 @@ return [
 
         'cloudinary' => [
             'driver' => 'cloudinary',
+            'url' => env('CLOUDINARY_URL'),
+            'cloud' => env('CLOUDINARY_CLOUD_NAME', ''),
+            'key' => env('CLOUDINARY_API_KEY', ''),
+            'secret' => env('CLOUDINARY_API_SECRET', ''),
         ],
 
     ],
