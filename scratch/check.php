6bbe801b39dac552479a412514b323a223930452
rasimin-max <rasimin85ori@gmail.com>
2026-09-03 21:26:20 +0700

    Catch Throwable in import

--- scratch/check.php
+++ scratch/check.php
@@ -1,13 +1,8 @@
 <?php
-$files = glob('app/Filament/Widgets/*.php');
-foreach ($files as $file) {
-    $content = file_get_contents($file);
-    if (strpos($content, 'extends ChartWidget') !== false) {
-        if (strpos($content, 'protected function getOptions(): array') !== false) {
-            preg_match('/protected function getOptions\(\): array\s*\{\s*return \[\s*\'(\w+)\' => \[/', $content, $matches);
-            echo $file . ' -> has getOptions, first key: ' . ($matches[1] ?? 'unknown') . PHP_EOL;
-        } else {
-            echo $file . ' -> NO getOptions' . PHP_EOL;
-        }
-    }
-}
+require 'vendor/autoload.php';
+$app = require_once 'bootstrap/app.php';
+$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
+$kernel->bootstrap();
+
+$permissions = Spatie\Permission\Models\Permission::pluck('name')->toArray();
+echo implode("\n", $permissions);
