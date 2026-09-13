b7472dd53dc2b245f75d73b734acc014ae9bb0fb
rasimin-max <rasimin85ori@gmail.com>
2026-09-13 14:10:20 +0700

    Add Sync Barcode button to ListMachineOperationRecords

--- app/Filament/Resources/MachineOperationRecordResource/Pages/ListMachineOperationRecords.php
+++ app/Filament/Resources/MachineOperationRecordResource/Pages/ListMachineOperationRecords.php
@@ -15,6 +15,26 @@ class ListMachineOperationRecords extends ListRecords
         return [
             \EightyNine\ExcelImport\ExcelImportAction::make()
                 ->color('primary'),
+            Actions\Action::make('sync_barcodes')
+                ->label('Sync Barcode')
+                ->icon('heroicon-o-arrow-path')
+                ->color('warning')
+                ->action(function () {
+                    $records = \App\Models\MachineOperationRecord::whereNull('barcode')->whereNotNull('machine_program_id')->get();
+                    $count = 0;
+                    foreach ($records as $record) {
+                        $program = \App\Models\MachineProgram::find($record->machine_program_id);
+                        if ($program && !empty($program->barcode)) {
+                            $record->barcode = $program->barcode;
+                            $record->saveQuietly();
+                            $count++;
+                        }
+                    }
+                    \Filament\Notifications\Notification::make()
+                        ->title("Berhasil menyinkronkan {$count} barcode!")
+                        ->success()
+                        ->send();
+                }),
             Actions\CreateAction::make(),
         ];
     }
