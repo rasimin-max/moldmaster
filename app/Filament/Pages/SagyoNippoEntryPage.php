50f757abc5157e9f22eb90ce7dd3fe7b1c34c646
rasimin-max <rasimin85ori@gmail.com>
2026-09-17 18:18:55 +0700

    fix: mold dropdown langsung tampil tanpa perlu klik - ganti live(false) ke preload()

--- app/Filament/Pages/SagyoNippoEntryPage.php
+++ app/Filament/Pages/SagyoNippoEntryPage.php
@@ -88,7 +88,7 @@ class SagyoNippoEntryPage extends Page implements HasForms, HasTable
                                     ->label('Mold')
                                     ->required()
                                     ->searchable()
-                                    ->live(false),
+                                    ->preload(),
                                 Forms\Components\Select::make('job_code_id')
                                     ->options(\App\Models\JobCode::all()->mapWithKeys(fn ($j) => [$j->id => "{$j->code} - {$j->item}"]))
                                     ->label('Job Code')
